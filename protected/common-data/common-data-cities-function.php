cff9ede3b0b70d36989a68af9800004cb728ea42
Fery Wardiyanto <ferywardiyanto@gmail.com>
2015-11-05 01:34:33 +0700

    Cleanup codebase 'common-data': Gak perlu pake array_push http://stackoverflow.com/a/559859/881743 Kita bisa panggil langsung semua object dari container tanpa harus manggil container itu sendiri
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- protected/common-data/common-data-cities-function.php
+++ protected/common-data/common-data-cities-function.php
@@ -2,21 +2,19 @@
 $app->get('/apps/common-data/cities/{province_id:[0-9]+}', function ($request, $response, $args) {
 
 	if ($request->isXhr()) {
-		$db = $this->getContainer()->get('db');
-
-		$q_cities = $db->createQueryBuilder()
-		->select('id', 'regional_name')
-		->from('regionals')
-		->where('parent_id = :pid')
-		->setParameter(':pid', $args['province_id'])
-		->execute();
+		$q_cities = $this->db->createQueryBuilder()
+			->select('id', 'regional_name')
+			->from('regionals')
+			->where('parent_id = :pid')
+			->setParameter(':pid', $args['province_id'])
+			->execute();
 
 		$cities = array();
 		foreach ($q_cities->fetchAll() as $item) {
-			array_push($cities, array(
+			$cities[] = array(
 				'id' => $item['id'],
 				'regional_name' => $item['regional_name']
-			));
+			);
 		}
 
 		return $response->withJson($cities, 200);
