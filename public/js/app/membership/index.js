357e25e7fe9e303dc6a0258f0cac51191d7793a6
Oka Prinarjaya <okaprinarjaya@gmail.com>
2015-11-07 02:40:27 +0700

    Fixing some unnecessary changes

--- public/js/app/membership/index.js
+++ public/js/app/membership/index.js
@@ -3,30 +3,24 @@ jQuery("#provinces-dd").on('change', function () {
 
     jQuery('#area').val('');
 
-    // disabled again if provinces value is null
-    if(prov_id == '') {
-        jQuery("#cities-dd").attr('disabled', 'disabled');
-    }
-
 	if (prov_id != '') {
         jQuery.ajax({
     		method: 'GET',
     		url: _base_url_+'/apps/common-data/cities/'+prov_id,
     		dataType: 'json'
     	}).done(function (data) {
+
             input_select({
                 value: 'id',
                 label: 'regional_name',
                 dst: jQuery("#cities-dd")
             }, data);
 
-            // enable select #cities-dd
-            jQuery("#cities-dd").removeAttr('disabled');
-
     	}).fail(function (data) {
     		alert('Failed to request data');
     	}).always(function (data) {
     	});
+        
     } else {
         input_select({dst: jQuery("#cities-dd")}, []);
     }
--- public/js/app/membership/register.js
+++ public/js/app/membership/register.js
@@ -1,11 +1,6 @@
 jQuery("#provinces-dd").on('change', function () {
     var prov_id = jQuery("#provinces-dd :selected").val();
 
-    // disabled again if provinces value is null
-    if(prov_id == '') {
-        jQuery("#cities-dd").attr('disabled', 'disabled');
-    }
-
 	if (prov_id != '') {
         jQuery.ajax({
     		method: 'GET',
@@ -18,13 +13,11 @@ jQuery("#provinces-dd").on('change', function () {
                 dst: jQuery("#cities-dd")
             }, data);
 
-            // enable select #cities-dd
-            jQuery("#cities-dd").removeAttr('disabled');
-
     	}).fail(function (data) {
     		alert('Failed to request data');
     	}).always(function (data) {
     	});
+        
     } else {
         input_select({dst: jQuery("#cities-dd")}, []);
     }
