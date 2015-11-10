62d19de7db45332bda7e63bb843eead5f6bc5b43
Fery Wardiyanto <ferywardiyanto@gmail.com>
2015-11-10 08:10:52 +0700

    Fix missing
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- protected/membership/membership-profile-edit-function.php
+++ protected/membership/membership-profile-edit-function.php
@@ -51,38 +51,25 @@ $app->map(['GET', 'POST'], '/apps/membership/profile/edit', function ($request, 
                     finfo_close($finfo);
 
                     $ext = strtolower(pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION));
-                    $new_fname = $_SESSION['MembershipAuth']['user_id'].'-'.date('YmdHis');
 
-                    try {
-                        $photo = \Cloudinary\Uploader::upload($_FILES['photo']['tmp_name'], [
-                            'public_id' => $new_fname,
-                            'tags' => ['user-avatar'],
-                        ]);
+                    if (in_array($mime_type, ['image/jpeg', 'image/png'])) {
+                        $new_fname = $_SESSION['MembershipAuth']['user_id'].'-'.date('YmdHis');
 
-                        $members_profiles['photo'] = $new_fname.'.'.$ext;
-                    } catch (\Cloudinary\Error $e) {
-                        $members_profiles['photo'] = null;
+                        try {
+                            $photo = \Cloudinary\Uploader::upload($_FILES['photo']['tmp_name'], [
+                                'public_id' => $new_fname,
+                                'tags' => ['user-avatar'],
+                            ]);
+
+                            $members_profiles['photo'] = $new_fname.'.'.$ext;
+                        } catch (\Cloudinary\Error $e) {
+                            $members_profiles['photo'] = null;
+                        }
                     }
 
                     if ($members_profiles['photo'] !== null) {
                         $_SESSION['MembershipAuth']['photo'] = $members_profiles['photo'];
                     }
-
-                    // if (($mime_type == 'image/jpeg' || $mime_type == 'image/png') && ($ext != 'php')) {
-                    //     $new_fname = $_SESSION['MembershipAuth']['user_id'].'-'.date('YmdHis').'.'.$ext;
-
-                    //     if (move_uploaded_file($_FILES['photo']['tmp_name'], $this->getContainer()->get('settings')['upload_photo_profile_path'].$new_fname)) {
-                    //         $members_profiles['photo'] = $new_fname;
-                    //         if ($_SESSION['MembershipAuth']['photo'] != null) {
-                    //             unlink($this->getContainer()->get('settings')['upload_photo_profile_path'].$_SESSION['MembershipAuth']['photo']);
-                    //         }
-
-                    //         $_SESSION['MembershipAuth']['photo'] = $new_fname;
-
-                    //     } else {
-                    //         $members_profiles['photo'] = null;
-                    //     }
-                    // }
                 }
 
                 $db->update('members_profiles', $members_profiles, array(
