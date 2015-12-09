1c98b6b2accbbfa325b5a854566a33d122a02168
chabibnr <chabibnurozak@gmail.com>
2015-12-09 12:52:54 +0700

    Enable google font

--- protected/views/layouts/layout-system.php
+++ protected/views/layouts/layout-system.php
@@ -19,12 +19,12 @@
 	<link rel="stylesheet" href="<?php echo $this->uri_base_url().'/public/css/meanmenu.css'; ?>" type="text/css" media="all" />
 	<link rel="stylesheet" href="<?php echo $this->uri_base_url().'/public/css/font-awesome.min.css'; ?>" type="text/css" />
     <link href="<?php echo $this->uri_base_url().'/public/css/formalize.css?v='.time(); ?>" rel="stylesheet">
-	<!--
+
 	<link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
 
 	<link rel="shortcut icon" href="<?php echo $this->uri_base_url().'/public/images/favicon.png'; ?>" type="image/png">
-	-->
+
 	<?php
 	if (isset($layout_use_captcha) && $layout_use_captcha == true):
 	?>
