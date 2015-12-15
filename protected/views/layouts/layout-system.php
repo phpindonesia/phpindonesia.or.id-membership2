0816870c57220e4a9d019d1b0bd4ee31d48ccc00
chabibnr <chabibnurozak@gmail.com>
2015-12-16 04:55:52 +0700

    fix

--- protected/views/layouts/layout-system.php
+++ protected/views/layouts/layout-system.php
@@ -18,19 +18,13 @@
 	<link rel="stylesheet" href="<?php echo $this->uri_base_url().'/public/css/responsive.css'; ?>" type="text/css" media="all" />
 	<link rel="stylesheet" href="<?php echo $this->uri_base_url().'/public/css/meanmenu.css'; ?>" type="text/css" media="all" />
 	<link rel="stylesheet" href="<?php echo $this->uri_base_url().'/public/css/font-awesome.min.css'; ?>" type="text/css" />
-<<<<<<< HEAD
-<<<<<<< 7edac41c9c42601461d9840d3667ac87d8b14901
     <link rel="stylesheet" href="<?php echo $this->uri_base_url().'/public/css/formalize.css?v='.time(); ?>" />
 	<link rel="stylesheet" href="<?php echo $this->uri_base_url().'/public/css/responsive2.css'; ?>" type="text/css" media="all" />
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic" type="text/css" />
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" type="text/css" />
-=======
->>>>>>> Add Bootstrap grid system
-=======
 
 	<link rel="shortcut icon" href="<?php echo $this->uri_base_url().'/public/images/favicon.png'; ?>" type="image/png">
 
->>>>>>> 0d49ae36e92a477cbb134cf782a787e49c18f4e5
 	<?php
 	if (isset($layout_use_captcha) && $layout_use_captcha == true):
 	?>
