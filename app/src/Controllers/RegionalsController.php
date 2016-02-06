51feca62b7e0792d82b24978845afd0b963d55bd
Tirta <web@astadala.com>
2016-02-06 11:14:10 +0800

    Fix error

--- app/src/Controllers/RegionalsController.php
+++ app/src/Controllers/RegionalsController.php
@@ -12,13 +12,13 @@ class RegionalsController extends Controllers
     {
         $this->assertXhrRequest($request, $response);
 
-        return $res->withJson($this->data(Regionals::class)->getCities(), 200);
+        return $response->withJson($this->data(Regionals::class)->getCities(), 200);
     }
 
     public function provinces(Request $request, Response $response, array $args)
     {
         $this->assertXhrRequest($request, $response);
 
-        return $res->withJson($this->data(Regionals::class)->getProvinces(), 200);
+        return $request->withJson($this->data(Regionals::class)->getProvinces(), 200);
     }
 }
