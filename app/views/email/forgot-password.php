b3d7fc33788918e6fc2216e05ad98c6b8df76d4e
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-02-09 16:37:32 +0700

    Initial usage
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/views/email/forgot-password.php
+++ app/views/email/forgot-password.php
@@ -1,17 +1,17 @@
-Haayy ^_^,
+Haayy <?php echo $this->e($fullname) ?> ^_^,
 
-Anda adalah pemilik akun dengan email {email_address} dan anda telah melakukan permintaan 
-perubahan password pada tanggal: {request_reset_date} karena anda lupa dengan password anda.
+Anda adalah pemilik akun dengan email <?php echo $this->e($email) ?>, dan anda telah melakukan permintaan
+perubahan password pada tanggal: <?php echo $this->e($reqDate) ?>, karena anda lupa dengan password anda.
 
 Kami butuh konfirmasi anda untuk memastikan bahwa anda memang benar-benar telah melakukan permintaan perubahan password sesuai kehendak anda.
 
-Jika permintaan perubahan password ini memang sesuai dengan kehendak anda, maka silahkan 
-klik link {base_url}{reset_path} untuk melakukan konfirmasi.
+Jika permintaan perubahan password ini memang sesuai dengan kehendak anda, maka silahkan
+klik link <?php echo $this->e($resetUrl) ?> untuk melakukan konfirmasi.
 
 Jika permintaan perubahan password ini bukan kehendak anda, maka email ini bisa diabaikan.
 Jika berkenan, Anda dapat melaporkannya kepada kami melalui email report@phpindonesia.or.id
 
-Batas waktu 2 jam dari sekarang untuk melakukan konfirmasi, dan url konfirmasi diatas akan kadaluarsa pada: {reset_expired_date}
+Batas waktu 2 jam dari sekarang untuk melakukan konfirmasi, dan url konfirmasi diatas akan kadaluarsa pada: <?php echo $this->e($resetExp) ?>
 
 
 Terimakasih ^_^
--- app/views/email/reset-password.php
+++ app/views/email/reset-password.php
@@ -1,11 +1,11 @@
-Haayy ^_^,
+Haayy <?php echo $this->e($fullname) ?> ^_^,
 
 Anda telah mengkonfirmasi permintaan reset password karena anda lupa dengan password anda.
 
 Berikut ini adalah password baru anda untuk sementara.
-password baru sementara: {temp_pwd}
+password baru sementara: <?php echo $this->e($tmpPass) ?>
 
-Mohon segera ganti password baru sementara ini dengan password baru pilihan anda. Pastikan 
+Mohon segera ganti password baru sementara ini dengan password baru pilihan anda. Pastikan
 mudah diingat dan aman!
 
 Terimakasih ^_^
