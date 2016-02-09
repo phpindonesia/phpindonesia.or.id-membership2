23df24b74a0558180916e582495be1c3ee7ee0fa
Choirul Afandi <science.afandi@gmail.com>
2016-02-10 00:27:44 +0700

    fix session store login

--- app/views/account-index.php
+++ app/views/account-index.php
@@ -36,7 +36,7 @@ $this->appendJs([
                             <li style="margin-right: 5px; padding: 3px; border: 1px #DDDDDD solid;">
                                 <a style="border: none; font: inherit;" href="<?php echo $socmed_item['account_url'] ?>">
                                     <span class="fa <?php echo $socmedias[$socmed_item['socmed_type']][1]; ?>"></span>
-                                    <?php echo !empty($socmed_item['account_name']) ? $socmed_item['account_name'] : $session['username'] ?>
+                                    <?php echo !empty($socmed_item['account_name']) ? $socmed_item['account_name'] : $member['username'] ?>
                                 </a>
                             </li>
 
