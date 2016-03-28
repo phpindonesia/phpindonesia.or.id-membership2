00b0350f02456ed03b99de31ad6c9c0b4f0764de
Fery Wardiyanto <ferywardiyanto@gmail.com>
2016-03-28 08:56:42 +0700

    Patch docblock
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- app/src/ContainerAware.php
+++ app/src/ContainerAware.php
@@ -10,7 +10,7 @@ use BadMethodCallException;
  * @property-read \Slim\PDO\Database db
  * @property-read \Valitron\Validator validator
  * @property-read \Projek\Slim\Plates view
- * @property-read \Membership\Utils\Mailer mailer
+ * @property-read \Membership\Libraries\Mailer mailer
  * @property-read callable data
  * @property-read callable upload
  * @method Models data(string $class)
